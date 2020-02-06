3865af19a760805210ca6125ff4206f4a018ad9d
Catharina <catharina.sorum@outlook.com>
2020-02-06 13:56:56 +0100

    Added prisjustering

--- oppgavesett01/index.js
+++ oppgavesett01/index.js
@@ -1,9 +1,41 @@
 // HTML elementer
+const moebelliste = document.querySelector("#moebelliste");
+const inpNavn = document.querySelector("#inpNavn");
+const inpType = document.querySelector("#inpType");
+const inpPris = document.querySelector("#inpPris");
+const inpRabatt = document.querySelector("#inpRabatt");
+const btnEndre = document.querySelector("#btnEndre");
+
+
 
 
 // Koblingen til databasen 
 const db = firebase.firestore(); 
 const mr = db.collection("moebelregister");
+
+const endrePris = (id) => {
+    const inpFelt = document.getElementById(id);
+    const nyPris = inpFelt.value;
+
+    mr.doc(id).update({
+        pris: nyPris
+    });
+};
+
+mr.onSnapshot(snap => {
+    moebelliste.innerHTML = "";
+
+    for(const moebel of snap.docs) {
+        moebelliste.innerHTML += `
+            <div> ${moebel.data().navn} </div>
+            <div> ${moebel.data().type} </div>
+            <div> ${moebel.data().pris} </div>
+            <input type="number" id="${moebel.id}">
+            <button onclick="endrePris('${moebel.id}')">Endre pris</button>
+            <div> ${moebel.data().rabatt} </div>
+        `;
+    }
+});
 
 // Funksjonen for å legge til møbel
 const leggtilMoebel = async (navn, type, pris, rabatt) => {
@@ -15,14 +47,33 @@ const leggtilMoebel = async (navn, type, pris, rabatt) => {
     });
 };
 
-// Hentet møbler fra Jysk
-leggtilMoebel("Vandborg", "Skrivebord", 599, 399);
-leggtilMoebel("Vandborg", "Reol", 799, 599);
-leggtilMoebel("Trappedal", "Reol", 899, 699);
-leggtilMoebel("Hejlsminde", "Vegghylle", 250, 250);
-leggtilMoebel("Dokkedal", "TV-bord", 699, 599);
+btnEndre.onclick = () => {
+    leggtilMoebel(inpNavn.value, inpType.value, inpPris.value, inpRabatt.value);
+    inpNavn.value = "";
+    inpType.value = "";
+    inpPris.value = ""; 
+    inpRabatt.value = "";
+    inpNavn.focus();
+};
+
+
+// Oppgave 2
+/*
+mr.doc("hpsTd7Fw7oxFOgm8MZU9").update({
+    pris: 3999
+});
+*/
+
+// Oppgave 1 - Hentet møbler fra Jysk
+/*
+leggtilMoebel("Vandborg", "Skrivebord", 599, 0);
+leggtilMoebel("Vandborg", "Reol", 799, 0);
+leggtilMoebel("Trappedal", "Reol", 899, 0);
+leggtilMoebel("Hejlsminde", "Vegghylle", 250, 0);
+leggtilMoebel("Dokkedal", "TV-bord", 699, 0);
 leggtilMoebel("Temprakon", "Kontinentalseng", 23000, 19999);
-leggtilMoebel("Halshuse", "Lenestol", 4699, 3999);
-leggtilMoebel("Egedal", "Sofa", 4499, 3999);
+leggtilMoebel("Halshuse", "Lenestol", 4699, 0);
+leggtilMoebel("Egedal", "Sofa", 4499, 0);
 leggtilMoebel("Virum", "Konsollbord", 899, 450);
-leggtilMoebel("Branderup", "Spisegruppe", 4999, 3895);
+leggtilMoebel("Branderup", "Spisegruppe", 4999, 0);
+*/
