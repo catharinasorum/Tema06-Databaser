15b602255d70141881de15734fedf0caedf6fb5e
Catharina <catharina.sorum@outlook.com>
2020-02-07 10:19:54 +0100

    added

---
+++ Firestore-og-svelte/superchat/index.js
@@ -0,0 +1,40 @@
+const user = "Catharina";
+
+const messageDiv = document.querySelector("#messageDiv");
+const messageForm = document.querySelector("#messageForm");
+const messageText = document.querySelector("#messageText");
+const inpFarge = document.querySelector("#inpFarge");
+const main = document.querySelector("main");
+
+
+
+// db = database
+const db = firebase.firestore();
+const chatten = db.collection("chatten");
+
+chatten.onSnapshot(snap => {
+    for( const message of snap.docChanges() ) {
+
+        if(message.type === "added") {
+            const melding = message.doc.data();
+            messageDiv.innerHTML += `
+                <div id="${message.id}">
+                    <span>${melding.from}:</span>
+                    <span>${melding.text}</span>
+                </div>
+            `;
+        };
+    };
+});
+
+// Hindrer submitten til å oppdatere siden på nytt når du sender skjemaet
+messageForm.onsubmit = (evt) => {
+    evt.preventDefault();
+
+    chatten.add({
+        from: user,
+        text: messageText.value,
+        time: firebase.firestore.FieldValue.serverTimestamp()
+    });
+};
+
