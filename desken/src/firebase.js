ab38d5ff8f2a296e2112131ba91399e5749a41b4
Catharina <catharina.sorum@outlook.com>
2020-02-13 10:07:06 +0100

    added firebase

---
+++ desken/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/storage"
+
+// Masse kode fra Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyChPiqiGebBUsFpofiUGW2Q9HyJst_jxJY",
+    authDomain: "firestore-2e7e1.firebaseapp.com",
+    databaseURL: "https://firestore-2e7e1.firebaseio.com",
+    projectId: "firestore-2e7e1",
+    storageBucket: "firestore-2e7e1.appspot.com",
+    messagingSenderId: "384392672001",
+    appId: "1:384392672001:web:8fb2e431d12c5e0329e100"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const storage = firebase.storage();
